f37ee8c3ebfcfb040ae92d712209502b00190e67
Janira Crispin <janiracrispin@gmail.com>
2022-12-07 15:33:44 -0500

    Modified Questions Class

--- questions_database.rb
+++ questions_database.rb
@@ -19,6 +19,11 @@ class Users
         @lname = options['lname']
     end 
 
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM users')
+        data.map { |datum| Users.new(datum)}
+    end 
+
     def self.find_by_id(id)
         id_found = QuestionsDatabase.instance.execute(<<-SQL)
         SQL
@@ -48,7 +53,7 @@ class Users
     end 
 end 
 
-class Questions
+class Question
 
     def initialize(options)
         @id = options['id']
@@ -57,4 +62,36 @@ class Questions
         @user_id = options['user_id']
     end 
 
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map { |datum| Question.new(datum)}
+    end 
+    
+    def self.find_by_id(id)
+        id_found = QuestionsDatabase.instance.execute(<<-SQL)
+        SQL
+    end 
+
+    def create
+        raise '#{self} already in database' if @id 
+        QuestionsDatabase.instance.execute(<<-SQL, @title, @body, @user_id)
+        INSERT INTO
+            users (title, body, user_id)
+        VALUES
+            (?, ?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end 
+
+    def update 
+        raise '#{self} not in database' unless id 
+        QuestionsDatabase.instance.execute(<<-SQL, @title, @body, @user_id, @id)
+            UPDATE
+                questions
+            SET
+                title = ?, body = ?, user_id = ?
+            WHERE
+                id = ?
+        SQL
+    end 
 end 
