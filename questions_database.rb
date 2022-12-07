6dd633d01cd9a4d119fe29dd768e5f5a6fb7de72
Janira Crispin <janiracrispin@gmail.com>
2022-12-07 16:37:56 -0500

    Completed phase 1

--- questions_database.rb
+++ questions_database.rb
@@ -13,6 +13,8 @@ end
 
 class Users
 
+    attr_accessor :fname, :lname, :id
+
     def initialize(options)
         @id = options['id']
         @fname = options['fname']
@@ -25,8 +27,15 @@ class Users
     end 
 
     def self.find_by_id(id)
-        id_found = QuestionsDatabase.instance.execute(<<-SQL)
+        id_found = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT 
+            * 
+        FROM 
+            users
+        WHERE id = ?
         SQL
+        return nil unless id_found.length > 0
+        User.new(id_found.first) 
     end 
 
     def create
@@ -55,6 +64,7 @@ end
 
 class Question
 
+    attr_accessor :id, :title, :body, :user_id
     def initialize(options)
         @id = options['id']
         @title = options['title']
@@ -68,8 +78,15 @@ class Question
     end 
     
     def self.find_by_id(id)
-        id_found = QuestionsDatabase.instance.execute(<<-SQL)
+        id_found = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT 
+            * 
+        FROM 
+            questions
+        WHERE id = ?
         SQL
+        return nil unless id_found.length > 0
+        Question.new(id_found.first) 
     end 
 
     def create
@@ -90,6 +107,111 @@ class Question
                 questions
             SET
                 title = ?, body = ?, user_id = ?
+            WHERE
+                id = ?
+        SQL
+    end 
+end 
+
+class Reply
+
+    attr_accessor :id, :reply, :parent_reply_id, :question, :user
+    def initialize(options)
+        @id = options['id']
+        @reply = options['reply']
+        @parent_reply_id = options['parent_reply_id']
+        @question = options['question']
+        @user = options['user']
+    end 
+
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM replies')
+        data.map { |datum| Reply.new(datum)}
+    end 
+    
+    def self.find_by_id(id)
+        id_found = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT 
+            * 
+        FROM 
+            replies
+        WHERE id = ?
+        SQL
+        return nil unless id_found.length > 0
+        Reply.new(id_found.first) 
+    end 
+
+
+    def create
+        raise '#{self} already in database' if @id 
+        QuestionsDatabase.instance.execute(<<-SQL, @reply, @parent_reply_id, @question, @user)
+        INSERT INTO
+            replies (reply, parent_reply_id, question, user)
+        VALUES
+            (?, ?, ?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end 
+
+    def update 
+        raise '#{self} not in database' unless id 
+        QuestionsDatabase.instance.execute(<<-SQL, @reply, @parent_reply_id, @question, @user, @id)
+            UPDATE
+                replies
+            SET
+                reply = ?, parent_reply_id = ?, question = ?, user = ?
+            WHERE
+                id = ?
+        SQL
+    end 
+end 
+
+class Like
+
+    attr_accessor :id, :like, :question, :user
+    def initialize(options)
+        @id = options['id']
+        @like = options['likes']
+        @question = options['question']
+        @user = options['user']
+    end 
+
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM question_likes')
+        data.map { |datum| Like.new(datum)}
+    end 
+    
+    def self.find_by_id(id)
+        id_found = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT 
+            * 
+        FROM 
+            question_likes
+        WHERE id = ?
+        SQL
+        return nil unless id_found.length > 0
+        Like.new(id_found.first) 
+    end 
+
+
+    def create
+        raise '#{self} already in database' if @id 
+        QuestionsDatabase.instance.execute(<<-SQL, @like, @question, @user)
+        INSERT INTO
+            questions_likes (likes, question, user)
+        VALUES
+            (?, ?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end 
+
+    def update 
+        raise '#{self} not in database' unless id 
+        QuestionsDatabase.instance.execute(<<-SQL, @like, @question, @user, @id)
+            UPDATE
+                question_likes
+            SET
+                likes = ?, question = ?, user = ?
             WHERE
                 id = ?
         SQL
